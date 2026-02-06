1eb3c33259cdc1fe43241237eca013ee03d286c1
allan716 <525223688@qq.com>
2026-02-06 16:17:48 +0800

    fix(01-04): fix sidebar layout overlap
    Move Sidebar outside of flex container and apply padding-left to
    main wrapper instead of main element to prevent overlap

--- web/src/layouts/MainLayout.tsx
+++ web/src/layouts/MainLayout.tsx
@@ -4,17 +4,17 @@ import { Sidebar } from "@/components/Sidebar";
 
 export default function MainLayout() {
   return (
-    <div className="flex h-screen flex-col">
-      <TopNav />
-      <div className="flex flex-1 overflow-hidden">
-        {/* Sidebar (fixed position, hidden on mobile) */}
-        <Sidebar />
+    <>
+      {/* Sidebar (fixed position, hidden on mobile) */}
+      <Sidebar />
 
-        {/* Main content area */}
-        <main className="flex-1 overflow-auto p-4 md:p-6 md:pl-80">
+      {/* Main content wrapper */}
+      <div className="flex h-screen flex-col md:pl-80">
+        <TopNav />
+        <main className="flex-1 overflow-auto p-4 md:p-6">
           <Outlet />
         </main>
       </div>
-    </div>
+    </>
   );
 }
