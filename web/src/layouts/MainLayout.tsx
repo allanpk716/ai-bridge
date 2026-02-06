bebd9bd6d5d57a4b2bfaeca11df5947b76972c88
allan716 <525223688@qq.com>
2026-02-06 15:52:45 +0800

    feat(01-04): add Sidebar to MainLayout
    - Import Sidebar component
    - Add Sidebar to left side of layout
    - Use flex layout: Sidebar (fixed) + Main content (flex-1)
    - Add responsive padding: main content has md:pl-80 to account for sidebar
    - Mobile: sidebar hidden (via CSS in Sidebar component)
    - Desktop: sidebar visible, main content offset

--- web/src/layouts/MainLayout.tsx
+++ web/src/layouts/MainLayout.tsx
@@ -1,13 +1,20 @@
 import { Outlet } from "react-router";
 import TopNav from "@/components/TopNav";
+import { Sidebar } from "@/components/Sidebar";
 
 export default function MainLayout() {
   return (
     <div className="flex h-screen flex-col">
       <TopNav />
-      <main className="flex-1 overflow-auto p-4 md:p-6">
-        <Outlet />
-      </main>
+      <div className="flex flex-1 overflow-hidden">
+        {/* Sidebar (fixed position, hidden on mobile) */}
+        <Sidebar />
+
+        {/* Main content area */}
+        <main className="flex-1 overflow-auto p-4 md:p-6 md:pl-80">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
