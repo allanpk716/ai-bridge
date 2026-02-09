f4a05c490b75e8bd7440998e5d187576d21344d1
allan716 <525223688@qq.com>
2026-02-09 15:06:49 +0800

    feat(05-02): integrate OfflineBanner in App.tsx
    - Import OfflineBanner from '@/components/pwa'
    - Render OfflineBanner inside ThemeProvider but before RouterProvider
    - Ensure banner appears above all other content (z-index in component)
    - Banner is visible on all routes when offline

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,12 @@ import { RouterProvider } from "react-router";
 import router from "@/router";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { ConnectionDialog } from "@/components/connection/ConnectionDialog";
+import { OfflineBanner } from "@/components/pwa";
 
 function App() {
   return (
     <ThemeProvider>
+      <OfflineBanner />
       <ConnectionDialog />
       <RouterProvider router={router} />
     </ThemeProvider>
